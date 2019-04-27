e278ad5900a561ed7828ca87e7d8f7ef788633be
stO-Orz <chenzhijun941213@gmail.com>
2019-04-27 11:57:39 +1000

    semi-auto script

---
+++ subnode.sh
@@ -0,0 +1,16 @@
+echo "== Set variables =="
+export node=115.146.92.10
+export user=admin
+export password=123456
+
+echo "== Start the containers =="
+docker run -d -p 5984:5984 -p 5986:5986 -p 4369:4369 -p 9100:9100 couchdb:2.3.0
+sleep 2
+
+declare -a cont=`docker ps | grep couchdb | cut -f1 -d' '`
+
+docker exec ${cont} bash -c "echo \"-setcookie couchdb_cluster\" >> /opt/couchdb/etc/vm.args"
+docker exec ${cont} bash -c "echo \"-name couchdb@${node}\" >> /opt/couchdb/etc/vm.args"
+
+docker restart ${cont[0]}
+sleep 3

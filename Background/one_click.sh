2bea24380650f5f48b6ab6f4d1a7aef496f35960
wenbin <wbchaw@qq.com>
2019-04-30 10:57:02 +1000

    change file

--- Background/one_click.sh
+++ Background/one_click.sh
@@ -15,7 +15,7 @@ curl -X PUT http://admin:123456@localhost:5984/twitter
 curl -X POST "http://admin:123456@localhost:5984/twitter/_bulk_docs " --header "Content-Type: application/json" --data @/home/ubuntu/90024/Background/MyCouchdb/twitter/melb.json
 curl -X PUT http://admin:123456@localhost:5984/geo_origin
 curl -X POST "http://admin:123456@localhost:5984/geo_origin " --header "Content-Type: application/json" --data @/home/ubuntu/90024/Background/MyCouchdb/twitter/origin_melb.json
-
+curl -X POST "http://admin:123456@localhost:5984/geo_origin " --header "Content-Type: application/json" --data @/home/ubuntu/90024/Background/MyCouchdb/twitter/victoria.json
 
 
 

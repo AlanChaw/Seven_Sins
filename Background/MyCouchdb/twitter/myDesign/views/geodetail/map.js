c87b23dfcc60e0a0e3fe2286312d668385b9ff69
wenbin <wbchaw@qq.com>
2019-05-01 22:15:47 +1000

    update geodetail API

--- Background/MyCouchdb/twitter/myDesign/views/geodetail/map.js
+++ Background/MyCouchdb/twitter/myDesign/views/geodetail/map.js
@@ -1,18 +1,26 @@
 function (doc) {
   // var count = 5000;
-  positive = Math.floor(Math.random() * 1000)
-  negative = Math.floor(Math.random() * 1000)
-  neutral = Math.floor(Math.random() * 1000)
-  total = positive + negative + neutral
+  positive = Math.floor(Math.random() * 1000);
+  negative = Math.floor(Math.random() * 1000);
+  neutral = Math.floor(Math.random() * 1000);
+  total = positive + negative + neutral;
   key = {
     "name": doc.properties.SA2_NAME16,
     // "cartodb_id": doc.properties.cartodb_id
-  }
+  };
+  pos_example = "I am a positive twitter example. Very Happy!";
+  neg_example = "I am a negative twitter example. F**k!";
+  neu_example = "I am a neutral twitter example. Whatever.";
   values = {
     "positive": positive,
     "negative": negative,
     "neutral": neutral,
-    "total": total
+    "total": total,
+    "examples":{
+      "pos_example": pos_example,
+      "neg_example": neg_example,
+      "neu_example": neu_example
+    }
   }
   emit(key, values);
 }

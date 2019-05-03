bf87ddd0fc63e5ecb013b23cc98260b9f22ae6d0
JackCoffee <31920676+JackCoffee@users.noreply.github.com>
2019-05-04 01:13:17 +1000

    harvester 2.0
    when deployed, auto-search and save tweets in Melb that have suburb or coordinates(trans to suburb) to couchdb
    to until last 10 days -  the maximum(then auto stop and exit)

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,84 +1,68 @@
-# # get tweepy api object
-# import tweepy
-# consumer_key = 'v0972w9aptiA8Dzm97sGFykPY'
-# consumer_secret = 'e9P4IMHyWEIwqwKiYVu4qDwXLMBooockFeqs5lwkjhCvJ0efle'
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
-# # access_key = '988266868828815360-LUDs2XCggWF57JVCZpOSvDTyxYNxEcg'
-# # access_secret = 'GdkzCMPkAFhJU6jHfr15ZD4TmSETTW41RQLxp3lNODoVL'
-# # auth.set_access_token(access_key, access_secret)
-# # api = tweepy.API(auth)
-
-# # connect to database
-# import couchdb
-# couch = couchdb.Server("http://115.146.92.183:5984")
-# database = couch['jan_to_apr_by_region']
-# database.save({"a": 123})
-
-
-# # get google map api object
-# import googlemaps
-# gmaps = googlemaps.Client(key='AIzaSyBJSHURhn2lPbsK8P1A1TK17XZvC_zTdCg')
-# print (gmaps.reverse_geocode((-37.81194167,144.95)))
-
-
-# api.search(q="haha", count=5, since_id=12345, max_id=54321)
-# search()[k].id  ==  search()[k]._json["id"] == 该推文的唯一id == 可进行since_id与max_id的比较
-# 从max_id往下走(时间变早, 往前回溯), 直到看到since_id或达到大约的count数量, 返回[since_id后一个,max_id]的tweetObjList
-# 每个15分钟内可call search()大约450次, 达到则自动卡最短时间直到下个时间段, 每次返回大约的min(count,100)的tweets,
-# q 为精确查找还是模糊查找?
-
-
-# c = tweepy.Cursor(api.search,q="haha").items()
-# iteration=0;error=0;place=0;coor=0;
-# file2 = open(r"result.txt","w+")
-# while True:
-#     if iteration==25000:break
-#     try:
-#         tweet = c.next()._json
-#         if tweet["coordinates"]!= None or tweet["place"]!= None:
-#             if tweet["coordinates"]!=None: coor+=1
-#             if tweet["place"]!=None:place+=1
-#             file2.write(json.dumps({"coor":tweet["coordinates"],"place":tweet["place"],"time":tweet["created_at"]}));file2.write("\n")
-#         iteration+=1;print(iteration)
-#     except tweepy.TweepError:print("too many requests, sleep 60 seconds");time.sleep(60)
-#     except StopIteration:print("break from stop"); break
-#     except:error+=1;
-# print ([iteration,error,place,coor])
-
-
-# # to save raw data
-# import json
-# import couchdb
-# couch = couchdb.Server("http://115.146.92.183:5984")
-# fileR = open('raw2018monthMel.json', 'rb')
-# i=0;jump=0;newLine = fileR.readline()
-# while newLine:
-#     i += 1;print (i)
-#     try:
-#         newLine = fileR.readline()
-#         tweetJS = json.loads(newLine.decode().strip().strip(','))
-#         if tweetJS["doc"]["coordinates"]:couch['month_by_coor'].save({"doc": tweetJS})
-#         elif tweetJS["doc"]["place"]:
-#             if tweetJS["doc"]["place"]["place_type"]=="poi": couch['month_by_coor'].save({"doc": tweetJS})
-#             elif tweetJS["doc"]["place"]["place_type"]=="neighborhood": couch['month_by_region'].save({"doc": tweetJS})
-#             elif tweetJS["doc"]["place"]["place_type"]=="city": couch['month_by_city'].save({"doc": tweetJS})
-#     except:jump+=1
-# print([i,jump])
-
-
-# to save cleaned data
+# -*- coding: utf-8 -*-
+import time
 import json
+import tweepy
 import couchdb
-couch = couchdb.Server("http://115.146.92.183:5984")
-database = couch['mar_by_region']
-fileR = open('nei2018marMel.json', 'rb')
-newLine = fileR.readline()
-while newLine:
-    database.save({"doc": json.loads(newLine)})
-    newLine = fileR.readline()
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
+# initialize database
+couch = couchdb.Server("http://100.103.123.77:5984/")
+database= couch['temp']
 
+# initialize apis
+key_secret_pairs=[];
+key_secret_pairs.append(('rcFJ0DzhHSDvTfgHK49WMCc9S','LZdhAUDVvnWcHHxUXTzYQF2f57KrNfmtEZHCmKzNZjxxsuG0Bp'))
+api_s=[]
+for key_secret_pair in key_secret_pairs:
+    auth = tweepy.OAuthHandler(key_secret_pair[0], key_secret_pair[1])
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api_s.append(api)
+
+# load boundary file
+boundaryJS = json.load(open('data/melb.json'))
+
+def cor2suburb(coordinates, boundaryJS):
+    for ele in boundaryJS["features"]:
+        if ele['geometry']['type']=='Polygon':
+            if Polygon(ele["geometry"]["coordinates"][0]).contains(Point(coordinates)):
+                return ele['properties']["SA2_NAME16"]
+        elif ele['geometry']['type']=='MultiPolygon':
+            for polygon in ele['geometry']['coordinates']:
+                if Polygon(polygon[0]).contains(Point(coordinates)):
+                    return ele['properties']["SA2_NAME16"]
+    return None
+
+def do100tweets(database, tweet_list):
+    # print(len(tweet_list))
+    for tweet in tweet_list[1:]:
+        tweetJS=tweet._json;
+        if tweetJS["place"]:
+            if tweetJS["place"]["place_type"]=="neighborhood":
+                database.save({"suburb": tweetJS["place"]["name"], "doc": tweetJS})
+                # print(tweetJS["place"]["name"])
+                continue
+        if tweetJS["coordinates"]:
+            long,lat = tweetJS["coordinates"]["coordinates"][0],tweetJS["coordinates"]["coordinates"][1]
+            sub = cor2suburb([long,lat],boundaryJS)
+            # print([long, lat, sub])
+            if sub!=None:
+                database.save({"suburb":sub,"doc":tweetJS})
+
+# newer tweet has bigger id
+# api.search(since_id=null, max_id=newest_id, count=min(100,count))
+# return [maxID, down to about_count]
+# or [max_id, down to the id just bigger than since_id]
+# or [max_id, least_id within 10 days] when reaching the last-10-day limit
+# each api can call search about 450 times within 15 mins, otherwise auto-wait least seconds until the next 15 mins
+tweet_list =api_s[-1].search(max_id =1123893169257631749,geocode="-37.999250,144.997395,57km", count=100)
+maxID = tweet_list[0].id
+while len(tweet_list)>1:
+    for api in api_s:
+        tweet_list = api.search(max_id=maxID, geocode="-37.999250,144.997395,57km", count=100);
+        try: do100tweets(database, tweet_list)
+        except: pass
+        maxID  = tweet_list[-1].id
 
 
 
@@ -102,8 +86,5 @@ while newLine:
 #
 # myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
 # myStream.filter(track=["*"],locations=[144.5937,-37.9994,145.4013,-37.5113])
-
-
-
 
 

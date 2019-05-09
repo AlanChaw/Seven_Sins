e54057123450a1e6b903bf0ba126b31d9d908332
JackCoffee <31920676+JackCoffee@users.noreply.github.com>
2019-05-10 01:06:39 +1000

    two versions of harverter
    save into database named 'temp', one for future(until 5000 valid tweets), one for past(until past 10 days -  the maximum).

---
+++ searcher.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+import json
+import tweepy
+import couchdb
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import re
+
+# global variables:
+# boundary file - dictionary
+boundaryJS = json.load(open('D:/data/melb.json'))
+# all suburb names - list
+sub_list=[ele['properties']["SA2_NAME16"] for ele in boundaryJS["features"]]
+# target database object
+database= couchdb.Server("http://100.103.123.77:5984/")['temp']
+# initialize apis
+key_secret_pairs=[];
+key_secret_pairs.append(('rcFJ0DzhHSDvTfgHK49WMCc9S','LZdhAUDVvnWcHHxUXTzYQF2f57KrNfmtEZHCmKzNZjxxsuG0Bp'))
+api_s=[]
+for key_secret_pair in key_secret_pairs:
+    auth = tweepy.OAuthHandler(key_secret_pair[0], key_secret_pair[1])
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api_s.append(api)
+
+def sub_name_normalisation(input_name):
+    for standard_sub in sub_list:
+        if input_name.lower() == standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.lower() in standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.replace(" ", " - ").lower() == standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.replace(" ", " - ").lower() in standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if ('South' not in standard_sub) and ('North' not in standard_sub) and ('West' not in standard_sub) and (
+                'East' not in standard_sub):
+            new = re.sub(r' South| North| West| East', '', input_name)
+            if new.lower() == standard_sub.lower():
+                return standard_sub
+    for standard_sub in sub_list:
+        if ('South' not in standard_sub) and ('North' not in standard_sub) and ('West' not in standard_sub) and (
+                'East' not in standard_sub):
+            new = re.sub(r' South| North| West| East', '', input_name)
+            if new.lower() in standard_sub.lower():
+                return standard_sub
+    return None
+
+def cor2suburb(coordinates):
+    for ele in boundaryJS["features"]:
+        if ele['geometry']['type']=='Polygon':
+            if Polygon(ele["geometry"]["coordinates"][0]).contains(Point(coordinates)):
+                return ele['properties']["SA2_NAME16"]
+        elif ele['geometry']['type']=='MultiPolygon':
+            for polygon in ele['geometry']['coordinates']:
+                if Polygon(polygon[0]).contains(Point(coordinates)):
+                    return ele['properties']["SA2_NAME16"]
+    return None
+
+def do100tweets(tweet_list):
+    print(len(tweet_list))
+    for tweet in tweet_list[1:]:
+        tweetJS=tweet._json;
+        if tweetJS["place"]:
+            if tweetJS["place"]["place_type"]=="neighborhood":
+                old_name = tweetJS["place"]["name"]
+                new_name = sub_name_normalisation(old_name)
+                if new_name !=None:
+                    database.save({"suburb": new_name, "doc": tweetJS})
+                    print(new_name)
+                    continue
+        if tweetJS["coordinates"]:
+            long,lat = tweetJS["coordinates"]["coordinates"][0],tweetJS["coordinates"]["coordinates"][1]
+            sub = cor2suburb([long,lat])
+            if sub!=None:
+                print([long, lat, sub])
+                database.save({"suburb":sub,"doc":tweetJS})
+
+# newer tweet has bigger id
+# api.search(since_id=null, max_id=newest_id, count=min(100,count))
+# return [maxID, down to about_count]
+# or [max_id, down to the id just bigger than since_id]
+# or [max_id, least_id within 10 days] when reaching the last-10-day limit
+# each api can call search about 450 times within 15 mins, otherwise auto-wait least seconds until the next 15 mins
+tweet_list =api_s[-1].search(geocode="-37.999250,144.997395,57km", count=100)
+maxID = tweet_list[0].id
+while len(tweet_list)>1:
+    for api in api_s:
+        tweet_list = api.search(max_id=maxID, geocode="-37.999250,144.997395,57km", count=100);
+        try: do100tweets(tweet_list)
+        except: pass
+        maxID  = tweet_list[-1].id
+
+
+
+
+
+
+
---
+++ streamer.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import json
+import tweepy
+import couchdb
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import re
+
+# global variables:
+# boundary file - dictionary
+boundaryJS = json.load(open('D:/data/melb.json'))
+# all suburb names - list
+sub_list=[ele['properties']["SA2_NAME16"] for ele in boundaryJS["features"]]
+# target database object
+database= couchdb.Server("http://100.103.123.77:5984/")['temp']
+
+def sub_name_normalisation(input_name):
+    for standard_sub in sub_list:
+        if input_name.lower() == standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.lower() in standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.replace(" ", " - ").lower() == standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if input_name.replace(" ", " - ").lower() in standard_sub.lower():
+            return standard_sub
+    for standard_sub in sub_list:
+        if ('South' not in standard_sub) and ('North' not in standard_sub) and ('West' not in standard_sub) and (
+                'East' not in standard_sub):
+            new = re.sub(r' South| North| West| East', '', input_name)
+            if new.lower() == standard_sub.lower():
+                return standard_sub
+    for standard_sub in sub_list:
+        if ('South' not in standard_sub) and ('North' not in standard_sub) and ('West' not in standard_sub) and (
+                'East' not in standard_sub):
+            new = re.sub(r' South| North| West| East', '', input_name)
+            if new.lower() in standard_sub.lower():
+                return standard_sub
+    return None
+
+def cor2suburb(coordinates):
+    for ele in boundaryJS["features"]:
+        if ele['geometry']['type']=='Polygon':
+            if Polygon(ele["geometry"]["coordinates"][0]).contains(Point(coordinates)):
+                return ele['properties']["SA2_NAME16"]
+        elif ele['geometry']['type']=='MultiPolygon':
+            for polygon in ele['geometry']['coordinates']:
+                if Polygon(polygon[0]).contains(Point(coordinates)):
+                    return ele['properties']["SA2_NAME16"]
+    return None
+
+def do1tweet(json_tweet):
+    print(json_tweet["coordinates"],json_tweet["place"])
+    if json_tweet["place"]:
+        if json_tweet["place"]["place_type"]=="neighborhood":
+            old_name = json_tweet["place"]["name"]
+            new_name = sub_name_normalisation(old_name)
+            if new_name !=None:
+                database.save({"suburb": new_name, "doc": json_tweet})
+                print(new_name)
+                return True
+    if json_tweet["coordinates"]:
+        long,lat = json_tweet["coordinates"]["coordinates"][0],json_tweet["coordinates"]["coordinates"][1]
+        sub = cor2suburb([long,lat])
+        if sub!=None:
+            database.save({"suburb":sub,"doc":json_tweet})
+            print([long, lat, sub])
+            return True
+    return False
+
+class MyStreamListener(tweepy.StreamListener):
+    i = 0
+    def on_data(self, data):
+        jsontweet=json.loads(data);
+        try:
+            bool = do1tweet(jsontweet)
+            if bool == True: self.i += 1
+        except:pass
+        if self.i>1000:return False
+
+auth= tweepy.OAuthHandler('rcFJ0DzhHSDvTfgHK49WMCc9S', 'LZdhAUDVvnWcHHxUXTzYQF2f57KrNfmtEZHCmKzNZjxxsuG0Bp')
+auth.set_access_token('1121779286069760000-UHzlxp96uTyQXjtHJFm5rLqzI0TRuH', 'yHrsQCrFWzlHhSyoPetkRW0ACNu4mzpSq1YRPkdZzgaFL')
+myStream = tweepy.Stream(auth = auth, listener=MyStreamListener())
+myStream.filter(locations=[144.293405,-38.548275,145.493112,-37.505479])

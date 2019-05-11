021c4e2dccd261d9a73516d239e81f12554b4bbd
stO-Orz <chenzhijun941213@gmail.com>
2019-05-12 01:03:28 +1000

    update apache server setup

--- assignment/run.sh
+++ assignment/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # apply the instances, render the templates
-. ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook --ask-become-pass apply-instance.yml
+# . ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook --ask-become-pass apply-instance.yml
 
 # install docker on the remote servers
-. ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v docker-configuration.yml
+# . ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v docker-configuration.yml
 
 # setup couchdb cluster on database servers
-. ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v couchdb-cluster-setup.yml
+# . ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v couchdb-cluster-setup.yml
+
+# setup the web server and load the data
+. ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v background-configuration.yml

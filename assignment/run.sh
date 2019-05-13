c5360157b0980e21175c638e5c5826ca6ef9294b
stO-Orz <chenzhijun941213@gmail.com>
2019-05-13 14:45:12 +1000

    Update run.sh

--- assignment/run.sh
+++ assignment/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # apply the instances, render the templates
-# . ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook --ask-become-pass apply-instance.yml
+. ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook --ask-become-pass apply-instance.yml
 
 # install docker on the remote servers
-# ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 docker-configuration.yml
+ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 docker-configuration.yml
 
 # reset the enviroment of the remote servers
-ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v environment-reset.yml
+# ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v environment-reset.yml
 
 # setup couchdb cluster on database servers
 ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 couchdb-cluster-setup.yml

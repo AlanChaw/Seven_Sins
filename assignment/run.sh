87cf574f8d14f87d58befdd9eae8ebe7fe91bd2f
stO-Orz <chenzhijun941213@gmail.com>
2019-05-13 10:16:54 +1000

    remove test code

--- assignment/run.sh
+++ assignment/run.sh
@@ -4,13 +4,10 @@
 . ./unimelb-comp90024-group-58-openrc.sh; ansible-playbook --ask-become-pass apply-instance.yml
 
 # install docker on the remote servers
-ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v docker-configuration.yml
-
-# reset the enviroment of the remote servers
-# ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v environment-reset.yml
+ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 docker-configuration.yml
 
 # setup couchdb cluster on database servers
-ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v couchdb-cluster-setup.yml
+ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 couchdb-cluster-setup.yml
 
 # setup the web server and load the data
-ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 -v background-configuration.yml
+ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 background-configuration.yml
